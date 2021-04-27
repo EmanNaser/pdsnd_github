7860004c00c2d1f5c4965c160a775373b73346b7
unknown <test@email.com>
2021-04-27 11:56:34 -0700

    refactor : change the massage of restarting the program

--- bikeshare.py
+++ bikeshare.py
@@ -225,7 +225,7 @@ def main():
             display_raw_data(df)
             break
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\n to restart, Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
